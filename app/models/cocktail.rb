e61c9bda5cd5e998811f92282fe34c410decd8b0
Deborah Sillmann <deborahsillmann@gmx.de>
2021-02-19 14:51:04 +0100

    picture upload from user

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,7 @@ class Cocktail < ApplicationRecord
   #delete doses if cocktail is deleted
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  has_one_attached :photo
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
